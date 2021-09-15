ae19d0f761da89f4ca4aef3a5ff78fb7612f7496
vyytx <dobeender@gmail.com>
2021-09-15 13:44:07 +0800

    manifest add

--- components/mainfest.js
+++ components/mainfest.js
@@ -1,10 +1,7 @@
-import Head from 'next/head'
-import styles from '../styles/layout.module.css'
-
 export default function Mainfest() {
     return (
-        <div className={styles.container}>
-
+        <div class="mainfest">
+            
         </div>
     )
 }
