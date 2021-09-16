723fae0db4ad15e4dd12b919c98ff600a2bf135c
vyytx <dobeender@gmail.com>
2021-09-16 20:49:44 +0800

    add mainfest todo and remove unused sass

--- components/layout/main/mainfest.js
+++ components/layout/main/mainfest.js
@@ -3,7 +3,9 @@ import layout from '../../../styles/layout.module.sass'
 export default function Mainfest() {
 	return (
 		<>
-			<div className={layout.mainfest}></div>
+			<div className={layout.mainfest}>
+				//TODO add some text 
+			</div>
 		</>
 	)
 }
