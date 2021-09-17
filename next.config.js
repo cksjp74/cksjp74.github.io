e2fa5146dfdf309cac313e94f3f3ad5e8cdc9264
Dogeon188 <dogcatdog188@gmail.com>
2021-09-17 10:06:36 +0800

    hashed class name

--- next.config.js
+++ next.config.js
@@ -1,53 +1,26 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
 	reactStrictMode: true,
 	sassOptions: {
 		includePaths: [path.join(__dirname, 'styles')],
 	},
+	webpack(config) {
+		config.module.rules[3].oneOf.forEach((moduleLoader) => {
+			Array.isArray(moduleLoader.use) &&
+			moduleLoader.use.forEach((l) => {
+				if (/(?<!post)css-loader/.test(l.loader)) {
+					const { getLocalIdent, ...others } = l.options.modules
+					l.options = {
+						...l.options,
+						modules: {
+							...others,
+							localIdentName: "[hash:base64:6]",
+						},
+					}
+				}
+			})
+		})
+		return config
+	}
 }
-
-
-// TODO: remove hash afer real classname
-// URL: https://github.com/vercel/next.js/discussions/14156
-// This should work but not working
-/*
-const path = require('path');
-
-const regexEqual = (x, y) =>
-  x instanceof RegExp &&
-  y instanceof RegExp &&
-  x.source === y.source &&
-  x.global === y.global &&
-  x.ignoreCase === y.ignoreCase &&
-  x.multiline === y.multiline;
-
-module.exports = {
-  webpack(config) {
-    const sassRules = config.module.rules
-      .find((rule) => typeof rule.oneOf === "object")
-      .oneOf.find(
-        (rule) =>
-          rule.sideEffects === false &&
-          regexEqual(rule.test, /\.module\.(scss|sass)$/)
-      );
-
-    sassRules.use = sassRules.use.map((rule) =>
-      rule.loader.includes(path.join(__dirname, "node_modules", "css-loader", "dist", "cjs.js"))
-        ? {
-            ...rule,
-            options: {
-              ...rule.options,
-              modules: {
-                ...rule.modules,
-                localIdentName: "[hash:base64:5]",
-              },
-            },
-          }
-        : rule
-    );
-
-    return config;
-  },
-};
-*/
