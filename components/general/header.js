300f5e7547af6855f135c74c0a28b4fe69a6f57a
vyytx <dobeender@gmail.com>
2021-09-19 00:41:47 +0800

    扮成絣

--- components/general/header.js
+++ components/general/header.js
@@ -1,5 +1,6 @@
 import styles from "../../styles/layout.module.sass"
 import $ from 'jquery'
+import React from "react"
 
 function toggleNav() {
 	let sidebar = $('#sidebar')
@@ -20,21 +21,74 @@ function toggleNav() {
 export function Header() {
 	return (
 		<header className={styles.header}>
-			<div style={{width: 0}}><button className={styles.openNav} onClick={toggleNav}/></div>
+			<div style={{width: 0}}><button className={styles.openNav} onClick={""}/></div>
 			<h1>建中高三日研</h1>
 		</header>
 	)
 }
 
-export function Sidebar() {
-	return (
-		<>
-			<div id="sidebar" className={styles.sidebar} data-opened="false">
-				<a className={styles.closeNav} onClick={toggleNav} href='javascript:void(0)'/>
-				<a href='/'>關於我們</a>
-				<a href='/lessons'>社團課程</a>
-			</div>
-			<div className={styles.curtain}/>
-		</>
-	)
+// export function Sidebar(props) {
+	
+// 	function toggle() {
+// 		let sidebar = $('.' + styles.sidebar);
+// 		let curtain = $('.' + styles.curtain)
+// 		if(this.state.opened) {
+// 			sidebar.css('width', '0')
+// 			curtain.css('visibility', 'hidden')
+// 			curtain.css('opacity', '0')
+// 		}else {
+// 			sidebar.css('width', '250px')
+// 			curtain.css('visibility', 'visible')
+// 			curtain.css('opacity', '.5')
+// 		}
+// 		props.opened = !props.opened;
+// 	}
+
+// 	return (
+// 		<>
+// 			<div id="sidebar" className={styles.sidebar} data-opened="false">
+// 				<a className={styles.closeNav} onClick={toggle} href='javascript:void(0)'/>
+// 				<a href='/'>關於我們</a>
+// 				<a href='/lessons'>社團課程</a>
+// 			</div>
+// 			<div className={styles.curtain}/>
+// 		</>
+// 	)
+// }
+
+export class Sidebar extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			opened: false
+		}
+	}
+
+	toggle() {
+		let sidebar = $(this);
+		let curtain = $('.' + styles.curtain)
+		if(this.state.opened) {
+			sidebar.css('width', '0')
+			curtain.css('visibility', 'hidden')
+			curtain.css('opacity', '0')
+		}else {
+			sidebar.css('width', '250px')
+			curtain.css('visibility', 'visible')
+			curtain.css('opacity', '.5')
+		}
+		this.state.opened = !this.state.opened;
+	}
+
+	render() {
+		return (
+			<>
+				<div id="sidebar" className={styles.sidebar} data-opened="false">
+					<a className={styles.closeNav} onClick={this.toggle} href='javascript:void(0)'/>
+					<a href='/'>關於我們</a>
+					<a href='/lessons'>社團課程</a>
+				</div>
+				<div className={styles.curtain}/>
+			</>
+		)
+	}
 }
