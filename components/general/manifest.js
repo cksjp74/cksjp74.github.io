24d1ee714c9206aefc16863a31d7760d14508082
vyytx <dobeender@gmail.com>
2021-09-19 03:45:31 +0800

    fix style and move manifest out

---
+++ components/general/manifest.js
@@ -0,0 +1,12 @@
+import styles from "../../styles/page/index.module.sass"
+import font from "../../styles/font.module.sass"
+
+export default function manifest() {
+    return (
+        <>
+            <div className={styles.manifest}>
+                <h1>Oh. So. Japan.</h1>
+            </div>
+        </>
+    )
+}
