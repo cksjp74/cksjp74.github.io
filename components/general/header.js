a27603e21387df1e071a56d3d881a14925568d88
Dogeon188 <dogcatdog188@gmail.com>
2021-09-19 01:51:40 +0800

    sidebar final(?) fix

--- components/general/header.js
+++ components/general/header.js
@@ -1,98 +1,44 @@
 import styles from "../../styles/layout.module.sass"
 import $ from 'jquery'
-import React from "react"
+import {Component} from "react"
 
-function toggleNav() {
-	let sidebar = $('#sidebar')
-	let curtain = $('.' + styles.curtain)
-	if (sidebar.attr('data-opened') === 'false') {
-		sidebar.attr('data-opened', 'true')
-		sidebar.css('width', '250px')
-		curtain.css('visibility', 'visible')
-		curtain.css('opacity', '.5')
-	} else {
-		sidebar.attr('data-opened', 'false')
-		sidebar.css('width', '0')
-		curtain.css('visibility', 'hidden')
-		curtain.css('opacity', '0')
-	}
-}
-
-export function Header() {
-	return (
-		<header className={styles.header}>
-			<div style={{width: 0}}><button className={styles.openNav}/></div>
-			<h1>建中高三日研</h1>
-		</header>
-	)
-}
-
-// export function Sidebar(props) {
-	
-// 	function toggle() {
-// 		let sidebar = $('.' + styles.sidebar);
-// 		let curtain = $('.' + styles.curtain)
-// 		if(this.state.opened) {
-// 			sidebar.css('width', '0')
-// 			curtain.css('visibility', 'hidden')
-// 			curtain.css('opacity', '0')
-// 		}else {
-// 			sidebar.css('width', '250px')
-// 			curtain.css('visibility', 'visible')
-// 			curtain.css('opacity', '.5')
-// 		}
-// 		props.opened = !props.opened;
-// 	}
-
-// 	return (
-// 		<>
-// 			<div id="sidebar" className={styles.sidebar} data-opened="false">
-// 				<a className={styles.closeNav} onClick={toggle} href='javascript:void(0)'/>
-// 				<a href='/'>關於我們</a>
-// 				<a href='/lessons'>社團課程</a>
-// 			</div>
-// 			<div className={styles.curtain}/>
-// 		</>
-// 	)
-// }
-
-export class Sidebar extends React.Component {
+export default class Header extends Component {
 	constructor(props) {
-		super(props);
-		this.state = {
-			opened: false
-		}
+		super(props)
+		this.state = {opened: false}
 	}
 
 	toggle = () => {
 		this.setState(prevState => {
-			let sidebar = $('.' + styles.sidebar);
-			let curtain = $('.' + styles.curtain);
-			if(prevState.opened) {
+			let sidebar = $('.' + styles.sidebar)
+			let curtain = $('.' + styles.curtain)
+			if (prevState.opened) {
 				sidebar.css('width', '0')
 				curtain.css('visibility', 'hidden')
 				curtain.css('opacity', '0')
-			}else {
+			} else {
 				sidebar.css('width', '250px')
 				curtain.css('visibility', 'visible')
 				curtain.css('opacity', '.5')
 			}
-			return {
-				opened: !prevState.opened
-			}
+			return {opened: !prevState.opened}
 		})
 	}
 
 	render() {
-		return (
-			<>
-				<div id="sidebar" className={styles.sidebar}>
-					<a className={styles.closeNav} onClick={this.toggle} href='javascript:void(0)'/>
-					<a href='/'>關於我們</a>
-					<a href='/lessons'>社團課程</a>
+		return (<>
+			<header className={styles.header}>
+				<div style={{width: 0}}>
+					<a className={styles.openNav} onClick={this.toggle}/>
 				</div>
-				<div className={styles.curtain}/>
-			</>
-		)
+				<h1>建中高三日研</h1>
+			</header>
+			<div className={styles.sidebar}>
+				<a className={styles.closeNav} onClick={this.toggle} href='javascript:void(0)'/>
+				<a href='/'>關於我們</a>
+				<a href='/lessons'>社團課程</a>
+			</div>
+			<div className={styles.curtain}/>
+		</>)
 	}
 }
--- components/layout.js
+++ components/layout.js
@@ -1,17 +1,12 @@
-import {Header, Sidebar} from "./general/header"
+import Header from "./general/header"
 
 import styles from '../styles/layout.module.sass'
 
 export default function Layout({children}) {
 	return (
 		<>
+			<Header/>
 			<div className={styles.container}>
-				<Header/>
-				<Sidebar>
-					{{
-						opened: false
-					}}
-				</Sidebar>
 				<main id="main">{children}</main>
 			</div>
 		</>
