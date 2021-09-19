68f3e8c750d3e7fb83995bb7a5d62940ce96cc3d
Dogeon188 <dogcatdog188@gmail.com>
2021-09-19 21:44:52 +0800

    すべてが日本。

--- components/general/manifest.js
+++ components/general/manifest.js
@@ -3,7 +3,7 @@ import styles from "../../styles/page/index.module.sass"
 export default function manifest() {
     return (
         <div className={styles.manifest}>
-            <h1>Oh. So. Japan.</h1>
+            <h1>すべてが日本。</h1>
         </div>
     )
 }
