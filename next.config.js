efdf75c0556c351fda351657f3b495f9cd3b7566
Dogeon188 <dogcatdog188@gmail.com>
2021-09-20 22:42:52 +0800

    fix manifest image

--- next.config.js
+++ next.config.js
@@ -5,21 +5,5 @@ module.exports = {
 	reactStrictMode: true,
 	sassOptions: {
 		includePaths: [path.join(__dirname, 'styles')],
-	},
-	// ref: https://github.com/webpack-contrib/css-loader#localidentname
-	webpack(config) {
-		config.module.rules[3].oneOf.forEach((moduleLoader) => {
-			Array.isArray(moduleLoader.use) &&
-			moduleLoader.use.forEach((l) => {
-				if (/(?<!post)css-loader/.test(l.loader)) {
-					const others = l.options.modules
-					l.options.modules = {
-						...others,
-						localIdentName: '[local]_[hash:base64:6]',
-					}
-				}
-			})
-		})
-		return config
 	}
 }
