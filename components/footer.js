56c968514d89fdedcd5b7a93cfaea03c9881f32b
Dogeon188 <dogcatdog188@gmail.com>
2021-09-22 13:41:15 +0800

    footer

---
+++ components/footer.js
@@ -0,0 +1,10 @@
+import styles from '../styles/layout.module.sass'
+
+export default function Footer() {
+	return (<>
+		<footer className={styles.footer}>
+			此網頁使用 <a href='https://nextjs.org/'>Next.js</a> 和 <a href='https://pages.github.com/'>Github Pages</a> 運行。<br/>
+			原始碼可於 <a href='https://github.com/cksjp74/cksjp74.github.io'>此處</a> 查看。
+		</footer>
+	</>)
+}
--- components/layout.js
+++ components/layout.js
@@ -1,6 +1,7 @@
 import Header from './header'
 
 import styles from '../styles/layout.module.sass'
+import Footer from './footer';
 
 export default function Layout({manifest, children, isHome}) {
 	return (<>
@@ -8,6 +9,7 @@ export default function Layout({manifest, children, isHome}) {
 		{manifest}
 		<div className={styles.container}>
 			<main id='main'>{children}</main>
+			<Footer/>
 		</div>
 	</>)
 }
