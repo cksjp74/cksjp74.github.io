44c3aa96c98d0816e3f1fd2ad838fae398db0f88
Dogeon188 <dogcatdog188@gmail.com>
2021-09-27 17:47:15 +0800

    SEO

--- components/head.js
+++ components/head.js
@@ -5,6 +5,17 @@ export default function Head({title}) {
 	return (
 		<_Head>
 			<title>CKSJP — {title}</title>
+			{/*Global site tag (gtag.js) - Google Analytics*/}
+			<script async src='https://www.googletagmanager.com/gtag/js?id=G-Z2CM6JFNK3'/>
+			<script
+				dangerouslySetInnerHTML={{
+					__html: `
+					window.dataLayer = window.dataLayer || [];
+					function gtag(){dataLayer.push(arguments);}
+					gtag('js', new Date());
+					
+					gtag('config', 'G-Z2CM6JFNK3');`
+				}}/>
 		</_Head>
 	)
 }
